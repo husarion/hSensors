835b4425656fee5d39c0b81ff765876dd7f375f1
Krystian Dużyński <krystian.duzynski@gmail.com>
2015-04-12 14:22:49 +0200

    moved examples

--- examples/hitechnic_accel.cpp
+++ examples/hitechnic_accel.cpp
@@ -1,25 +1,23 @@
 #include <hFramework.h>
 #include <stdio.h>
 
-#include "hitechnic-accelerometer.h"
+#include "Hitechnic_Accel.h"
 
 using namespace hFramework;
+using namespace hSensors;
 
 void hMain(void)
 {
 	sys.setLogDev(&Serial);
 	
-	tHitechnicSensor sensor1(hSens1);
-	tHitechnicSensor sensor2(hSens2);
+	Hitechnic_Accel sensor(hSens1);
 	
 	for (;;)
 	{
-		int x, y, z;
-		int x2, y2, z2;
-		HTACreadAllAxes(sensor1, x, y, z);
-		HTACreadAllAxes(sensor2, x2, y2, z2);
+		int16_t x, y, z;
+		sensor.readRaw(x, y, z);
 		LED1.toggle();
-		printf("x %5d y %5d z %5d | x %5d y %5d z %5d\r\n", x, y, z, x2, y2, z2);
+		printf("x %5d y %5d z %5d\r\n", x, y, z);
 		sys.delay_ms(10);
 	}
 }
